952814c2dcd61d423bd179d6b83b2e288a6b8bce
Manh Pham <manhpham90vn@icloud.com>
2022-10-05 09:03:36 +0700

    Initial Commit

---
+++ Shared/Push_NotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Push_NotifyApp.swift
+//  Shared
+//
+//  Created by Manh Pham on 05/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Push_NotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
